113231e0de391945cf09b0e697f65e14e9c35aec
Raquel <ralvhdez@gmail.com>
2022-06-18 16:19:58 +0200

    adding api url in env config

--- src/config/client.ts
+++ src/config/client.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance } from "axios";
 
 const api: AxiosInstance = axios.create({
-  baseURL: process.env.API_URL,
+  baseURL: process.env.REACT_APP_API_URL,
   headers: {
     "Content-Type": "application/json",
   },
