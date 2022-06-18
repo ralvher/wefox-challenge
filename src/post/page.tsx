08e2f184291f8d3b2fbb0823edb1ff4a6d169a95
Raquel <ralvhdez@gmail.com>
2022-06-18 18:57:47 +0200

    add post provider inside post page

--- src/post/page.tsx
+++ src/post/page.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 
+import { PostProvider } from "./context";
+
 const Page = () => {
   return (
-    <div style={{ display: "flex", flexDirection: "column" }}>
-      <Outlet />
-    </div>
+    <PostProvider>
+      <div style={{ display: "flex", flexDirection: "column" }}>
+        <Outlet />
+      </div>
+    </PostProvider>
   );
 };
 
