f16db4e1b2708d4d4cd72c388683773719f08569
Raquel <ralvhdez@gmail.com>
2022-06-21 18:17:49 +0100

    details page component

--- src/post/Details.tsx
+++ src/post/Details.tsx
@@ -1,7 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
+import { useParams } from "react-router-dom";
+import Loading from "../ui/Loading";
+import PostCard from "./components/PostCard";
+import usePosts from "./context";
+import { Post } from "./context/PostModel";
 
 const PostDetail = () => {
-  return <div>Post details</div>;
+  const params = useParams();
+  const { isLoading, getById, edit, remove } = usePosts();
+  const [editable, setEditable] = useState(false);
+
+  const post = getById(params?.id || "");
+
+  const handleEditPost = async (post: Post) => {
+    setEditable(true);
+  };
+
+  const handleSavePost = async (post: Post) => {
+    await edit(post);
+    setEditable(false);
+  };
+
+  const handleRemovePost = async (post: Post) => {
+    await remove(post);
+    setEditable(false);
+  };
+
+  if (isLoading) return <Loading />;
+  if (!post) return <div>Post not found</div>;
+
+  return (
+    <PostCard
+      post={post}
+      editable={editable}
+      onRemove={handleRemovePost}
+      onSave={handleSavePost}
+      onEdit={handleEditPost}
+      onCancel={() => setEditable(false)}
+    />
+  );
 };
 
 export default PostDetail;
